f3ff6499e7c104b0d9a6d6447cd00fa454442f27
nageshkamoji <nagesh.kamoji1710@gmail.com>
2016-10-21 12:22:51 +0530

    Create ProjectTest.java

---
+++ src/test/java/com/example/ProjectTest.java
@@ -0,0 +1,13 @@
+package com.example;
+import junit.framework.TestCase;
+
+public class ProjectTest extends TestCase {
+
+    public Actionwords actionwords = new Actionwords();
+
+    public void testFindingASiteOnGoogle() {
+        actionwords.iOpenP1("http://google.com");
+        actionwords.iSearchForP1("Hiptest");
+        actionwords.aLinkToP1IsShownInTheResults("Hiptest - Agile test management tool");
+     }
+}
