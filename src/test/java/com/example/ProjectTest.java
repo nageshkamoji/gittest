5fde4fcd0dc4bc55c280bd642289d6caf1a3bd67
nageshkamoji <nagesh.kamoji1710@gmail.com>
2016-10-26 17:59:37 +0530

    Update ProjectTest.java

--- src/test/java/com/example/ProjectTest.java
+++ src/test/java/com/example/ProjectTest.java
@@ -4,26 +4,15 @@ import junit.framework.TestCase;
 
 public class ProjectTest extends TestCase {
 
-    Actionwords action = new Actionwords();
-
-    public void testGoogle() {
+     public void testGoogle() {
         System.out.println("inside testGoogle");
         
-        if (action != null)
-        {
-            // Given  I open "http://www/google.com"
-            action.iOpenP1("http://www/google.com");
-            // When I search for "Hiptest"
-            action.iSearchForP1("Hiptest");
-        }
-        else
-        {
           Actionwords action1 = new Actionwords();
+            System.out.println(action1);
+        
           action1.iOpenP1("http://www/google.com");
           // When I search for "Hiptest"
           action1.iSearchForP1("Hiptest");
-            
-        }
-        
+          
     }
 }
