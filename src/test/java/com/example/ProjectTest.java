6ce87011cc1b0db9608fa40091e52eeb84d3a360
nageshkamoji <nagesh.kamoji1710@gmail.com>
2016-10-26 19:44:38 +0530

    Update ProjectTest.java

--- src/test/java/com/example/ProjectTest.java
+++ src/test/java/com/example/ProjectTest.java
@@ -3,18 +3,13 @@ package com.example;
 import junit.framework.TestCase;
 
 public class ProjectTest extends TestCase {
-
-     public void testGoogle() {
-          
-             System.out.println("inside testGoogle");
-        
-            Actionwords action1 = new Actionwords();
-            System.out.println(action1);
-        
-           action1.iOpenP1("http://www/google.com");
-           System.out.println("after iOpenP1");
-          
-          action1.iSearchForP1("Hiptest");
-          System.out.println("after iSearchForP1");
-     }
+     
+     WebDriver driver=null;
+     
+     public void testP1(String p1) 
+	{
+	     String url = "http://www.google.com";
+          driver = new FirefoxDriver();
+          driver.get(url);
+	}
 }
