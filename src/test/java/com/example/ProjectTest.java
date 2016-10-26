536bc5d68d2595513b48f65eeb89256ff9c739c6
nageshkamoji <nagesh.kamoji1710@gmail.com>
2016-10-26 19:48:14 +0530

    Update ProjectTest.java

--- src/test/java/com/example/ProjectTest.java
+++ src/test/java/com/example/ProjectTest.java
@@ -1,5 +1,11 @@
 package com.example;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import junit.framework.TestCase;
 
 public class ProjectTest extends TestCase {
