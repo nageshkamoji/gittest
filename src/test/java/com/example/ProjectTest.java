e96681ed811b6084fc81617a2e63fcfcc5e31ba4
nageshkamoji <nagesh.kamoji1710@gmail.com>
2016-10-26 17:22:12 +0530

    Update ProjectTest.java

--- src/test/java/com/example/ProjectTest.java
+++ src/test/java/com/example/ProjectTest.java
@@ -2,15 +2,16 @@ package com.example;
 
 import junit.framework.TestCase;
 
-public class ProjectTest extends TestCase 
-{
+public class ProjectTest extends TestCase {
 
-    Actionwords actionwords = new Actionwords();
+    public Actionwords actionwords = new Actionwords();
 
-    public void testFindingASiteOnGoogle() 
-	{
-        actionwords.iOpenP1("http://google.com");
+    public void testFindingASiteOnGoogle() {
+        // Given  I open "http://www/google.com"
+        actionwords.iOpenP1("http://www/google.com");
+        // When I search for "Hiptest"
         actionwords.iSearchForP1("Hiptest");
-       
-     }
+        // Then a link to "Hiptest" agile management tool will open
+        //actionwords.aLinkToP1AgileManagementToolWillOpen("Hiptest");
+    }
 }
