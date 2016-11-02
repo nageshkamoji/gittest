235c0797d37bb8a3acf1744e9fd35e97cbe66e4a
nageshkamoji <nagesh.kamoji1710@gmail.com>
2016-11-02 16:01:43 +0530

    Add files via upload

--- src/test/java/com/example/Actionwords.java
+++ src/test/java/com/example/Actionwords.java
@@ -9,14 +9,30 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 
 public class Actionwords {
-     
-     WebDriver driver;
 	
-	public void iOpenP1(String p1) 
-	{
-	   driver = new FirefoxDriver();
-	   System.out.println("inside iOpenP1"+p1);	
-     	  driver.get(p1);
-	  System.out.println("after driver get");	
+    WebDriver driver=null;
+
+	public Actionwords() {
+		 driver = new FirefoxDriver();
 	}
+	
+	 public void iOpenP1(String p1) {
+		 driver.get(p1);
+	 }
+     public void iSearchForP1(String p1) {
+		 WebElement element = driver.findElement(By.name("q"));
+		 element.clear();
+		 element.sendKeys(p1);
+		 element.submit();
     }
+
+ public void aLinkToP1IsShownInTheResults(String p1) {
+    final String matcher = p1;
+
+    (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+        public Boolean apply(WebDriver d) {
+        return d.findElements(By.linkText(matcher)).size() != 0;
+        }
+    });
+   }
+}
--- src/test/java/com/example/ProjectTest.java
+++ src/test/java/com/example/ProjectTest.java
@@ -1,23 +1,15 @@
 package com.example;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+
 import junit.framework.TestCase;
 
 public class ProjectTest extends TestCase {
-     
-        Actionwords actionwords = new Actionwords();
-	
-	 System.out.println("BEFORE test FindingASiteOnGoogle");
-     
-     public void testFindingASiteOnGoogle(String p1) 
-	{
-	     System.out.println("inside testFindingASiteOnGoogle");
-		actionwords.iOpenP1("http://www/google.com");
-	  System.out.println("after iOpenP1");
-	}
+
+    Actionwords actionwords = new Actionwords();
+
+    public void testFindingASiteOnGoogle() {
+        actionwords.iOpenP1("http://google.com");
+        actionwords.iSearchForP1("Hiptest");
+        actionwords.aLinkToP1IsShownInTheResults("Hiptest - Agile test management tool");
+     }
 }
