e1bb7a8b1867e88c1281c941a6273151c70307b2
nageshkamoji <nagesh.kamoji1710@gmail.com>
2016-11-03 16:49:03 +0530

    Update Actionwords.java

--- src/test/java/com/example/Actionwords.java
+++ src/test/java/com/example/Actionwords.java
@@ -7,34 +7,38 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedCondition;
-import java.lang.String,java.lang.String;
-
-public class Actionwords {
+public class Actionwords 
+{
+	WebDriver driver=null;
+	public Actionwords() 
 	
-    WebDriver driver=null;
-
-	public Actionwords() {
+	{
 		System.setProperty("D:\\geckodriver-v0.11.1-win64\\geckodriver.exe");
 
 		driver = new FirefoxDriver();
 		
 	}
 	
-	 public void iOpenP1(String p1) {
+	 public void iOpenP1(String p1) 
+	 {
 		 driver.get(p1);
 	 }
-     public void iSearchForP1(String p1) {
+     public void iSearchForP1(String p1)
+     	 {
 		 WebElement element = driver.findElement(By.name("q"));
 		 element.clear();
 		 element.sendKeys(p1);
 		 element.submit();
-    }
+   	 }
 
- public void aLinkToP1IsShownInTheResults(String p1) {
+ public void aLinkToP1IsShownInTheResults(String p1)
+ {
     final String matcher = p1;
 
-    (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
-        public Boolean apply(WebDriver d) {
+    (new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() 
+	{
+        public Boolean apply(WebDriver d) 
+	{
         return d.findElements(By.linkText(matcher)).size() != 0;
         }
     });
